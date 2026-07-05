97e3ceb792878179c7ad87f72f38b5f8ff30c156
hassaanmuneer031-crypto <hassaanmuneer031@gmail.com>
2026-07-05 16:45:11 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,91 @@
-console.log("hello world");
-let name = "Talal";
-console.log(name);
+// while () {
+    // code block to be executed
+// }
+
+
+
+// let i = 0;
+
+// while (i <= 10) {
+//     console.log(i);
+//     i++
+// }
+
+
+
+
+
+// do {
+//   // code block to be executed
+// }
+// while (condition);
+
+
+// let i = 0;
+
+// do {
+//     console.log("world war 3");
+//     i++;
+// }
+// while (i < 10)
+
+
+
+
+
+
+// DataType []
+// Array
+// ["Talal", "Ahmed", 10, true]
+
+
+
+// length = 1 and index = 0
+// let human = ["Talal", "Ahmed"];
+// let human1 = ["Talal", "Ahmed"];
+
+
+// console.log(human1)
+// console.log(arr[0] + " " + arr[1]);
+
+
+
+
+// let i = 0;
+
+// i += 1;
+
+// console.log(i);
+
+
+
+
+// let i = 0;
+// let foods = ["Nihari", "Biryani", "Korma", "pae"];
+// let text = "";
+
+// while (foods[i]) {
+//     text += foods[i];
+//     text += ", ";
+//     i++;
+// }
+// console.log(foods[4]);
+
+
+
+
+
+
+
+
+
+// Array Methods
+
+let fruits = ["Banana", "Apple", "Mango", "Grapes", "Strawberry", "Orange"]
+// fruits.pop();
+// fruits.push("Blackberry");
+// fruits.shift();
+v// fruits.unshift("Watermelon");
+// fruits.splice(2, 0, "kiwi");
+fruits.splice(4  , 1 , "kiwi" , "blueberry")
+console.log(fruits);
