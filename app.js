fa310c22044f8e8e89671eb1f469c43d767af990
hassaanmuneer031-crypto <hassaanmuneer031@gmail.com>
2026-07-17 01:52:58 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,91 +1,34 @@
-// while () {
-    // code block to be executed
+// [1,5,12,15];
+
+// let arr = ;
+
+// function sumArr (arr) {
+//     let total = 0;
+
+//     for (let i = 0; i < arr.length; i++) {
+//         total = total - arr[i];
+//     }
+
+//     console.log(total);
 // }
 
+// sumArr([1,5,12,15])
+// sumArr([1,5,12,25])
 
 
-// let i = 0;
 
-// while (i <= 10) {
-//     console.log(i);
-//     i++
+
+// function getLargestNumber(arr){
+//     let largestNum = arr[0];
+//     for(let i = 0; i<arr.length; i++){
+//         if(arr[i] > largestNum){
+//             largestNum = arr[i];
+//         }
+//     }
+//     console.log("Largest Number : ", largestNum);
 // }
 
+// getLargestNumber([1,51,7,11,28]);
 
 
-
-
-// do {
-//   // code block to be executed
-// }
-// while (condition);
-
-
-// let i = 0;
-
-// do {
-//     console.log("world war 3");
-//     i++;
-// }
-// while (i < 10)
-
-
-
-
-
-
-// DataType []
-// Array
-// ["Talal", "Ahmed", 10, true]
-
-
-
-// length = 1 and index = 0
-// let human = ["Talal", "Ahmed"];
-// let human1 = ["Talal", "Ahmed"];
-
-
-// console.log(human1)
-// console.log(arr[0] + " " + arr[1]);
-
-
-
-
-// let i = 0;
-
-// i += 1;
-
-// console.log(i);
-
-
-
-
-// let i = 0;
-// let foods = ["Nihari", "Biryani", "Korma", "pae"];
-// let text = "";
-
-// while (foods[i]) {
-//     text += foods[i];
-//     text += ", ";
-//     i++;
-// }
-// console.log(foods[4]);
-
-
-
-
-
-
-
-
-
-// Array Methods
-
-let fruits = ["Banana", "Apple", "Mango", "Grapes", "Strawberry", "Orange"]
-// fruits.pop();
-// fruits.push("Blackberry");
-// fruits.shift();
-v// fruits.unshift("Watermelon");
-// fruits.splice(2, 0, "kiwi");
-fruits.splice(4  , 1 , "kiwi" , "blueberry")
-console.log(fruits);
+console.log(document);
